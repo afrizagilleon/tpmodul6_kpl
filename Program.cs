bd4f5dd12c2147ddae0dcf8e357637c6fce9e96d
Afriza Gilleon Ginting <afriza.gilleon@gmail.com>
2025-03-22 11:50:09 +0700

    add programs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace tpmodul6_103022300154
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
